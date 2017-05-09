b3408cb795f83a35eec7c2740f3e82522aa16863
Dan Parry <parryd424@gmail.com>
2017-05-09 09:14:30 -0400

    facebook complete

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,2 +1,26 @@
 class ArtsController < ApplicationController
+
+  def index
+    @arts = Art.all
+  end
+
+  def show
+    @art = Art.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def new
+    @art = Art.new
+  end
+
+  def create
+    @art = Art.create(art_params)
+    redirect_to arts_path
+  end
+
+  private
+
+  def art_params
+    params.require(:art).permit(:title, :price, :description, :user_id )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   resources :transactions
   resources :arts
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root( 'arts#index' )
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/sessions', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+
 end
